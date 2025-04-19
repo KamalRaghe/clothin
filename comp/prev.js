12a631c776c4e7fffe6dde4ecb0af8b0df1b0f0c
Kamal <kamal.raghe33@gmail.com>
2025-04-19 16:56:46 -0400

    commit

--- comp/prev.js
+++ comp/prev.js
@@ -1,11 +1,10 @@
-import { useRouter } from "next/router";
 import React, { useState } from "react";
-import Slider from "react-slick";
+
 
 function Previous({pic,button}) {
   return (
     <div>
-      <div>
+      <div onClick={button} >
         <img style={{width:"20vw"}} src={pic} ></img>
       </div>
     </div>
