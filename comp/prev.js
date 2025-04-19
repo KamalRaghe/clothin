d6064ef9ba15c1ef10ed23c095bbc81eaa4c46d1
Kamal <kamal.raghe33@gmail.com>
2025-04-19 17:15:04 -0400

    commit

--- comp/prev.js
+++ comp/prev.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
+import { MdNavigateNext } from "react-icons/md";
 
-
-function Previous({pic,button}) {
+function Previous({pic,button,next}) {
   return (
     <div>
-      <div onClick={button} >
+      <div className="center" onClick={button} >
+        {!next && <div style={{rotate:"180deg",fontSize:"100px",color:"grey"}} ><MdNavigateNext /></div>}
         <img style={{width:"20vw"}} src={pic} ></img>
+        {next && <div style={{fontSize:"100px",color:"grey"}} ><MdNavigateNext /></div>}
       </div>
     </div>
   );
