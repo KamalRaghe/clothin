3a9ef6c0efcf13d6f1c83295a4c9a9207ef0c919
Kamal <kamal.raghe33@gmail.com>
2025-04-19 17:19:15 -0400

    commit

--- comp/prev.js
+++ comp/prev.js
@@ -4,10 +4,9 @@ import { MdNavigateNext } from "react-icons/md";
 function Previous({pic,button,next}) {
   return (
     <div>
-      <div className="center" onClick={button} >
-        {!next && <div style={{rotate:"180deg",fontSize:"100px",color:"grey"}} ><MdNavigateNext /></div>}
+      <div className="center column" onClick={button} >
         <img style={{width:"20vw"}} src={pic} ></img>
-        {next && <div style={{fontSize:"100px",color:"grey"}} ><MdNavigateNext /></div>}
+        <div style={{margin:"20px"}} >Click to view</div>
       </div>
     </div>
   );
