500ca14efcb4c7df9744540e4a1de79bc952dc19
Kamal <kamal.raghe33@gmail.com>
2025-04-19 16:37:38 -0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,16 @@ import Slider from "react-slick";
 
 function SimpleSlider() {
  const [arr, setArr] = useState(['TrackPic1.jpeg',"TrackPic2.jpeg","TrackPic3.jpeg"]) 
+ function Next(){
+    arr.push(arr.shift())  
+ }
+ 
   return (
     <div>
       <div style={{height:"100vh"}} className="center column">
           <div className="center" >
-            <img style={{width:"20vw"}} src={arr[0]} ></img>
-            <img style={{width:"40vw",margin:"20px"}} src={arr[1]} ></img>
+            <img style={{width:"20vw"}} src={arr[1]} ></img>
+            <img style={{width:"40vw",margin:"20px"}} src={arr[0]} ></img>
             <img style={{width:"20vw"}} src={arr[2]} ></img>
           </div>
         <div><button>Prev</button> <button>Next</button></div>
