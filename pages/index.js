334bcda39153c52b1b398c078452c05ad172dd7b
Kamal <kamal.raghe33@gmail.com>
2025-04-19 16:43:22 -0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,29 @@
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 import Slider from "react-slick";
 
 function SimpleSlider() {
- const [arr, setArr] = useState(['TrackPic1.jpeg',"TrackPic2.jpeg","TrackPic3.jpeg"]) 
+ const [arr, setArr] = useState(['TrackPic1.jpeg',"TrackPic2.jpeg","TrackPic3.jpeg",'TrackPic4.jpeg',"TrackPic5.jpeg","TrackPic6.jpeg"])
+ const router = useRouter() 
  function Next(){
-    arr.push(arr.shift())  
+    arr.push(arr.shift()) 
+    router.push('/') 
  }
- 
+ function Prev(){
+  arr.unshift(arr.pop())
+ }
   return (
     <div>
       <div style={{height:"100vh"}} className="center column">
           <div className="center" >
-            <img style={{width:"20vw"}} src={arr[1]} ></img>
+            <img style={{width:"20vw"}} src={arr[arr.length-1]} ></img>
             <img style={{width:"40vw",margin:"20px"}} src={arr[0]} ></img>
-            <img style={{width:"20vw"}} src={arr[2]} ></img>
+            <img style={{width:"20vw"}} src={arr[1]} ></img>
           </div>
-        <div><button>Prev</button> <button>Next</button></div>
+        <div>
+          <button onClick={Prev} >Prev</button> 
+          <button onClick={Next} >Next</button>
+        </div>
       </div>
     </div>
   );
