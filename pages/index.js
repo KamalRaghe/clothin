42c4e2262f2d54a8db64481623ddca59b7205622
Kamal <kamal.raghe33@gmail.com>
2025-04-19 16:27:59 -0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,17 @@ import React from "react";
 import Slider from "react-slick";
 
 function SimpleSlider() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+
   return (
-    <div style={{height:"100vh"}} className="center column">
-        <div className="center" >
-          <img style={{width:"20vw"}} src="TrackPic1.jpeg" ></img>
-          <img style={{width:"40vw",margin:"20px"}} src="TrackPic2.jpeg" ></img>
-          <img style={{width:"20vw"}} src="TrackPic3.jpeg" ></img>
-        </div>
-       <div><button>Prev</button> <button>Next</button></div>
+    <div>
+      <div style={{height:"100vh"}} className="center column">
+          <div className="center" >
+            <img style={{width:"20vw"}} src="TrackPic1.jpeg" ></img>
+            <img style={{width:"40vw",margin:"20px"}} src="TrackPic2.jpeg" ></img>
+            <img style={{width:"20vw"}} src="TrackPic3.jpeg" ></img>
+          </div>
+        <div><button>Prev</button> <button>Next</button></div>
+      </div>
     </div>
   );
 }
